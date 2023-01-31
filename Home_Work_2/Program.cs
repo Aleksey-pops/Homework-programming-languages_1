82604fd799910d4a5b55e8f52ef57e70b5c2c5d4
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-01 01:05:15 +0300

    Output the name of the given day of the week.

--- Home_Work_2/Program.cs
+++ Home_Work_2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int First = int.Parse(Console.ReadLine());
+
+string[] days_of_the_week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",};
+
+Console.WriteLine($"День недели:  {days_of_the_week[First - 1]}");
