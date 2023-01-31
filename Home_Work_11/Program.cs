0e208e192d47813d7b1c8c7c6f75a30f94cb7d2d
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-01 00:18:40 +0300

    Find out if the number is a multiple of the given one, if not, print the remainder.

--- Home_Work_11/Program.cs
+++ Home_Work_11/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите 1 число: ");
+double First = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число: ");
+double Second = double.Parse(Console.ReadLine());
+
+if (First % Second == 0)
+{
+Console.WriteLine("Ура! Числа является кратными, друг другу.");
+}
+if (Second % First == 0 && Second / First > 1)
+{
+Console.WriteLine("Ура! Числа является кратными.");
+}
+if (First >= Second)Console.WriteLine("Числа не является кратными друг другу. Остаток от деления первого числа на второе: " + First % Second);
+if (Second >= First)Console.WriteLine("Числа не является кратными друг другу. Остаток от деления второго числа на первое: " + Second % First);
+
