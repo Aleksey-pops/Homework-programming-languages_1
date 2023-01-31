6634d2175873f5c11049ae5a8a84400142100159
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-31 20:46:34 +0300

    Given two numbers, check whether the first is the square of the second.

--- Home_Work_1/Program.cs
+++ Home_Work_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите 1 число: ");
+int First = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число: ");
+int Second = int.Parse(Console.ReadLine());
+
+int c = (First * First);
+if (Second == c )
+{
+    Console.WriteLine("Ура! Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Увы первое число не является квадратом второго.");
+
+}
