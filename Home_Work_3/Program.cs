0d799a7d2c4aa0e4351b422c280ebf45c3e686ea
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-31 21:57:47 +0300

    Finding the maximum of three numbers.

--- Home_Work_3/Program.cs
+++ Home_Work_3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ###  Найти максимальное из трех чисел
+Console.WriteLine("Найти максимальное из трех чисел!");
+
+Console.WriteLine("Введите 1 число: ");
+int First = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число: ");
+int Second = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 1 число: ");
+int True = int.Parse(Console.ReadLine());
+
+int max = First;
+
+if (First > Second) max = First;
+if (Second > max) max = Second;
+if (True > max) max = True;
+
+Console.Write("Максимальное число из трех равно: ");
+Console.WriteLine(max);
