8aa50fb650f33dde7ed5f7de5afd0359821dd0e4
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-05 23:08:00 +0300

    Remove the second digit of a three-digit number.

--- Home_Work_10/Program.cs
+++ Home_Work_10/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Удалить вторую цифру трёхзначного числа.
+
+
+Console.WriteLine("Введите трехзначное число:" ?? "0");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num3 <1000 && num3 >99)
+{
+    
+    Console.WriteLine("Третья цифра трехзначного числа: " + (num3 % 10)); int three = num3 % 10;
+    // Console.WriteLine(three);
+    num3 = (num3 / 10);
+    Console.WriteLine("Вторая цифра трехзначного числа: " + (num3 % 10));
+    num3 = (num3 / 10);
+    Console.WriteLine("Первая цифра трехзначного числа: " + (num3 % 10)); int first = num3 % 10;
+
+    Console.WriteLine("Трехзначное число без после удаления среднего числа: " + (first * 10 + three));
+}
+else
+{
+    Console.WriteLine("Неправильная цифра");
+}
+
