279107b7c84cca0945c56f83bf2d3591c718e755
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-05 20:07:37 +0300

    Find the third digit of a number or report that it is not there.

--- Home_Work_12/Program.cs
+++ Home_Work_12/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. ***Найти третью цифру числа или сообщить, что её нет***
+
+Console.WriteLine("Введите число:" ?? "0");
+int num2 = int.Parse(Console.ReadLine());
+while (num2 < 100)
+{
+    Console.WriteLine("Третья цифра отсутсвует в числе!");
+    break;
+} 
+
+    for (;num2 >= 1000 || num2 <100; num2 = num2 / 10)   
+{
+
+}
+    Console.WriteLine($"Третья цифра числа " + (num2 % 10));  
+ 
--- Home_Work_6/Program.cs
+++ Home_Work_6/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// ## 6. Показать четные числа от 1 до N
+// int num = 1;
+// Console.WriteLine("Введите число N: ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine(num);
+// for (int num = 1;num == N; num++);
+// //         if(i % 2 == 0);
+// // else
+// // {
+// Console.WriteLine(num);
+// // }
+// // else
+// // {
+    
+// // }
+// // {
+    
+//     Console.WriteLine(N);
+// // }
+int i = 6;
+while (i > 0)
+{
+    Console.WriteLine(i);
+    i--;
+}
