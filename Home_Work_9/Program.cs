0dd6652fe22d7c88616e372f5cb52a7ac102d7e0
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-05 22:20:48 +0300

    Given a number from the interval [10, 99]. Show the highest digit of a number.

--- Home_Work_9/Program.cs
+++ Home_Work_9/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+// for (int i = 10; i <= 99; i++)
+// {
+//     // Console.WriteLine(i);
+//     Console.WriteLine("Число от 10 до 99: " + (i));
+//     int first = i / 10;
+//     int second = i % 10;
+//     if (first > second)
+//         {   
+//             Console.WriteLine("Первое число больше второго: " + (first));
+//         }
+//         else
+//         {
+//             Console.WriteLine("Второе число больше первого: " + (i % 10));
+//         }
+// }
+
+
+// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа. Через рандомное число.
+
+
+Random rnd = new Random();
+int i = rnd.Next(10,100);
+    Console.WriteLine("Рандомное число от 10 до 99: " + "Это число: " + (i));
+    int first = i / 10;
+    int second = i % 10;
+    if (first > second)
+      {   
+        Console.WriteLine("Первое число больше второго: " + (first));
+        }
+    else
+    {
+        Console.WriteLine("Второе число больше первого: " + (i % 10));
+    }
