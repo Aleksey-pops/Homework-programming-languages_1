dfa1bd04a8c74bdfaa17d7d57fbc99f17558c6ca
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-05 20:56:54 +0300

    Show even numbers from 1 to N.

--- Home_Work_6/Program.cs
+++ Home_Work_6/Program.cs
@@ -1,26 +1,11 @@
 ﻿using System;
 // ## 6. Показать четные числа от 1 до N
-// int num = 1;
-// Console.WriteLine("Введите число N: ");
-// int num = int.Parse(Console.ReadLine());
-// Console.WriteLine(num);
-// for (int num = 1;num == N; num++);
-// //         if(i % 2 == 0);
-// // else
-// // {
-// Console.WriteLine(num);
-// // }
-// // else
-// // {
-    
-// // }
-// // {
-    
-//     Console.WriteLine(N);
-// // }
-int i = 6;
-while (i > 0)
+
+Console.WriteLine("Введите число N: " ?? "0");
+int N = int.Parse(Console.ReadLine());
+
+for (int num = 1; N > num || num % 2 == 0 ;num++)
+ if (num % 2 == 0)
 {
-    Console.WriteLine(i);
-    i--;
+    Console.WriteLine("Четные числа." + num);
 }
