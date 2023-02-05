60aacc3a70385642db022fe8c25e237dca189d5e
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-05 21:15:20 +0300

    Show the last digit of a three-digit number.

--- Home_Work_7/Program.cs
+++ Home_Work_7/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число:" ?? "0");
+int num2 = int.Parse(Console.ReadLine());
+if (num2 <1000 && num2 >99)
+{
+    Console.WriteLine($"Последняя цифра трехзначного числа: " + (num2 % 10));
+}
+else
+{
+    Console.WriteLine("Неправильная цифра");
+}
